d9672c13b21f7d17cb8807bb3ced7e8890ccbd36
Davi Marques <davi.amqs@hotmail.com>
2023-06-15 20:14:49 -0300

    upload to github

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ReactGA from "react-ga4";
+
+function App() {
+  // ReactGA.initialize(import.meta.env.VITE_GOOGLE_ANALYTICS_ID);
+  // ReactGA.send({
+  //   hitType: "pageview",
+  //   page: window.location.pathname + window.location.search,
+  // });
+
+  return <h1 className="text-3xl font-bold underline">Boilerplate Frontend</h1>;
+}
+
+export default App;
